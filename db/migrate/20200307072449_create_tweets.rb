07028732f3d9fcec2f827c6609d7d600f5154bb4
kaito0402 <kaitoseibo04@gmail.com>
2020-03-07 16:51:27 +0900

    create table

--- db/migrate/20200307072449_create_tweets.rb
+++ db/migrate/20200307072449_create_tweets.rb
@@ -1,7 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-
+      t.text :lyric, null: false
+      t.string :artist
+      t.text :link
+      t.text :text
       t.timestamps
     end
   end
