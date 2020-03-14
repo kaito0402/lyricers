c09c11d47c85a3b683be9d1421cbabfa9a9c0ab7
kaito0402 <kaitoseibo04@gmail.com>
2020-03-14 16:05:43 +0900

    結局searchのルーティングまでやっちゃった。

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   root "tweets#index"
   resources :tweets, only: [:index, :new, :create, :destroy, :show] do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 end
