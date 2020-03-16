1d257233c431f5d63e6bf7f909aaafabbd1afd2b
kaito0402 <kaitoseibo04@gmail.com>
2020-03-16 19:07:04 +0900

    いいね機能のモデルまで作っちゃった。

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,9 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    return Tweet.all unless search
+    Tweet.where('lyric LIKE(?) OR artist LIKE(?) OR song_name LIKE(?)',"%#{search}%","%#{search}%","%#{search}%")
+  end
 end
